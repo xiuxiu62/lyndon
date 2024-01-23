a447767dbe34e7e6d86af8670c056e505618abd3
xiuxiu62 <jacremer@live.com>
2024-01-23 09:19:31 -0800

    adds more gates

--- src/gate.rs
+++ src/gate.rs
@@ -47,3 +47,127 @@ pub fn hadamard<const N: usize>() -> Gate<N> {
             .unwrap(),
     )
 }
+
+pub fn pauli_x<const N: usize>() -> Gate<N> {
+    Gate::new(
+        (0..N)
+            .map(|i| {
+                (0..N)
+                    .map(|j| Complex::new(if i == j { 0.0 } else { 1.0 }, 0.0))
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap(),
+    )
+}
+
+pub fn pauli_y<const N: usize>() -> Gate<N> {
+    Gate::new(
+        (0..N)
+            .map(|i| {
+                (0..N)
+                    .map(|j| {
+                        let sign = match i {
+                            value if value == j => Complex::new(0.0, 1.0),
+                            _ => Complex::new(0.0, -1.0),
+                        };
+
+                        sign * Complex::new(0.5, 0.0)
+                    })
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap(),
+    )
+}
+
+pub fn pauli_z<const N: usize>() -> Gate<N> {
+    Gate::new(
+        (0..N)
+            .map(|i| {
+                (0..N)
+                    .map(|j| {
+                        let sign = if i == j { 1.0 } else { -1.0 };
+
+                        Complex::new(sign, 0.0)
+                    })
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap(),
+    )
+}
+
+pub fn cnot<const N: usize>(control: usize, target: usize) -> Gate<N> {
+    Gate::new(
+        (0..N)
+            .map(|i| {
+                (0..N)
+                    .map(|j| match i {
+                        _ if i == j => Complex::new(1.0, 0.0),
+                        _ if i == control && i == target => Complex::new(0.0, 1.0),
+                        _ => Complex::new(0.0, 0.0),
+                    })
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap(),
+    )
+}
+
+pub fn swap<const N: usize>(lhs: usize, rhs: usize) -> Gate<N> {
+    let matrix = (0..N)
+        .map(|i| {
+            (0..N)
+                .map(|j| match i {
+                    value if value == j => Complex::new(1.0, 0.0),
+                    _ if (i == lhs && j == rhs) || (i == rhs && j == lhs) => Complex::new(0.0, 1.0),
+                    _ => Complex::new(0.0, 0.0),
+                })
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap()
+        })
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+
+    Gate::new(matrix)
+}
+
+pub fn toffoli<const N: usize>(control_1: usize, control_2: usize, target: usize) -> Gate<N> {
+    Gate::new(
+        (0..N)
+            .map(|i| {
+                (0..N)
+                    .map(|j| match i {
+                        _ if i == j => Complex::new(1.0, 0.0),
+                        _ if i == target && j == target => Complex::new(0.0, 1.0),
+                        _ if (i == control_1 && j == control_1)
+                            || (i == control_2 && j == control_2) =>
+                        {
+                            Complex::new(0.0, 1.0)
+                        }
+                        _ => Complex::new(0.0, 0.0),
+                    })
+                    .collect::<Vec<_>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap(),
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,18 @@ mod register;
 use register::QuantumRegister;
 
 fn main() {
-    let mut register = QuantumRegister::<2>::new();
+    let mut register = QuantumRegister::<3>::new();
     let hadamard = gate::hadamard();
+    let cnot = gate::cnot(0, 1);
+    let toffoli = gate::toffoli(0, 1, 2);
+    let pauli_x = gate::pauli_x();
 
     dbg!(register);
     register.apply(&hadamard);
+    dbg!(register);
+    register.apply(&cnot);
+    register.apply(&pauli_x);
+    dbg!(register);
+    register.apply(&toffoli);
     dbg!(register);
 }
